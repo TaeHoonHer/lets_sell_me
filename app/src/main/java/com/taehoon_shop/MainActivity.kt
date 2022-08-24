002b78f70517744e06c1835f5c3db6907fc52e7d
taehoon <taehoon.her@gmail.com>
2022-08-24 15:40:21 +0900

    화면 전환

--- app/src/main/java/com/taehoon_shop/MainActivity.kt
+++ app/src/main/java/com/taehoon_shop/MainActivity.kt
@@ -2,45 +2,22 @@ package com.taehoon_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-
-private const val TAG = "MainActivity"
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate")
-    }
 
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart")
-    }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
+        bottomNavigationView.itemIconTintList = null
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart")
-    }
-
-    override fun onResume(){
-        super.onResume()
-        Log.d(TAG, "onResume")
-    }
-
-    override fun onPause(){
-        super.onPause()
-        Log.d(TAG, "onPause")
-    }
-
-    override fun onStop(){
-        super.onStop()
-        Log.d(TAG, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy")
+        val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
+        navController?.let {
+            bottomNavigationView.setupWithNavController(it)
+        }
     }
 }
